d3005afb587726aeb6082fe4210be95ec7524405
FDHLINSA <workshopinsa@gmail.com>
2024-06-02 16:25:58 +0000

    static

--- APrak/APrak/settings.py
+++ APrak/APrak/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "main"
+    "main",
 ]
 
 MIDDLEWARE = [
@@ -80,6 +80,7 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+STATIC_URL = '/static/'
 
 
 # Password validation
@@ -116,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
